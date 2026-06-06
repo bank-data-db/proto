209fdc2a2e6f311256a5dc55a48ab1a159b13134
Lucy Dryaeva <48590492+ShadiestGoat@users.noreply.github.com>
2026-06-06 23:42:23 +0100

    Add some fun utils

---
+++ utils/main.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"slices"
+
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type GenericRespList[T any] interface {
+	HasPaginationToken() bool
+	GetPaginationToken() string
+
+	GetResult() []T
+	GetTotalCount() uint32
+}
+
+func ListAll[T any](fetch func(tok *string) (GenericRespList[T], error)) ([]T, error) {
+	resp, err := fetch(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]T, resp.GetTotalCount())
+	copied := copy(res, resp.GetResult())
+	if !resp.HasPaginationToken() {
+		return res, nil
+	}
+
+	tok := new(resp.GetPaginationToken())
+
+	for {
+		resp, err := fetch(tok)
+		if err != nil {
+			return res, err
+		}
+		copied += copy(res[copied:], resp.GetResult())
+		if !resp.HasPaginationToken() {
+			return res, nil
+		}
+
+		tok = new(resp.GetPaginationToken())
+	}
+}
+
+func CopyTo(dst protoreflect.Message, src protoreflect.Message, forceSet bool, ignore ...string) {
+	dstFields := dst.Descriptor().Fields()
+	srcFields := src.Descriptor().Fields()
+	for i := range dstFields.Len() {
+		dstF := dstFields.Get(i)
+		if slices.Contains(ignore, string(dstF.Name())) {
+			continue
+		}
+		srcF := srcFields.ByName(dstF.Name())
+		if dstF == nil || srcF == nil {
+			continue
+		}
+		if forceSet || src.Has(srcF) {
+			dst.Set(dstF, src.Get(srcF))
+		}
+	}
+}
